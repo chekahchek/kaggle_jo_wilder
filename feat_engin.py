831e77f197a4d0849a79b628a09f3baa38d1a23a
Ho Chek Hui <chekhui@gmail.com>
2023-03-25 06:55:18 +0000

    New version

--- feat_engin.py
+++ feat_engin.py
@@ -196,7 +196,8 @@ def get_answer_time_1(df, train=True):
 
 def get_answer_time_2(stage3_df, stage2_path=None, retained_features=None, train=True):
     if train:
-        data = pd.read_csv(stage2_path, dtype={'session_id':'object', 'elapsed_time':np.int32})
+        # data = pd.read_csv(stage2_path, dtype={'session_id':'object', 'elapsed_time':np.int32})
+        data = pd.read_parquet(stage2_path)
         stage2_answertime = data.groupby('session_id').nth(-1)[['elapsed_time']]
         stage3_answertime = stage3_df.groupby('session_id').nth(0)[['elapsed_time']]
 
