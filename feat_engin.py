4a123067c983ec538bed511394e06e01765f21f0
Ho Chek Hui <chekhui@gmail.com>
2023-04-25 13:33:24 +0000

    New version

--- feat_engin.py
+++ feat_engin.py
@@ -112,27 +112,6 @@ def get_general_features_1(df, stage, train=True):
         *[pl.col('action_time').filter(pl.col('level') == i).std().alias(f"action_timestd_{i}") for i in levels],
         *[pl.col('action_time').filter(pl.col('level') == i).median().alias(f"action_timemedian_{i}") for i in levels],
         *[pl.col('action_time').filter(pl.col('level') == i).max().alias(f"action_timemax_{i}") for i in levels],
-        
-        
-        #Movment X 
-        pl.col('movement_x').max().alias('max_movement_x'),
-        pl.col('movement_x').mean().alias('mean_movement_x'),
-        pl.col('movement_x').std().alias('std_movement_x'),
-        
-        #Movement Y
-        pl.col('movement_y').max().alias('max_movement_y'),
-        pl.col('movement_y').mean().alias('mean_movement_y'),
-        pl.col('movement_y').std().alias('std_movement_y'),
-        
-        #Movement per each event name
-        *[pl.col('movement_x').filter(pl.col('event_name') == i).sum().alias(f"{i}_movement_x_sum") for i in EVENT_NAMES],
-        *[pl.col('movement_x').filter(pl.col('event_name') == i).mean().alias(f"{i}_movement_x_mean") for i in EVENT_NAMES],
-        *[pl.col('movement_x').filter(pl.col('event_name') == i).std().alias(f"{i}_movement_x_std") for i in EVENT_NAMES],
-        
-        *[pl.col('movement_y').filter(pl.col('event_name') == i).sum().alias(f"{i}_movement_y_sum") for i in EVENT_NAMES],
-        *[pl.col('movement_y').filter(pl.col('event_name') == i).mean().alias(f"{i}_movement_y_mean") for i in EVENT_NAMES],
-        *[pl.col('movement_y').filter(pl.col('event_name') == i).std().alias(f"{i}_movement_y_std") for i in EVENT_NAMES],
-        
     ]
 
     df = df.groupby(['session_id'], maintain_order=True).agg(aggs).sort('session_id')
@@ -254,35 +233,35 @@ def get_general_features_2(df, stage, train=True):
     if stage == 1:
         cols_needed = ['0_gramps', '0_notebook', '0_teddy', '1_groupconvo', '2_cs', '2_gramps', '2_tunic', '2_tunic.hub.slip', '3_plaque', '3_plaque.face.date', '3_toentry', '3_togrampa', '3_tomap', 
                        '4_toentry']
-    elif stage == 2:
-        cols_needed = ['5_toentry', '5_what_happened', '6_archivist','6_gramps', '6_magnify', '6_trigger_coffee', '6_trigger_scarf', '7_businesscards', '7_businesscards.card_0.next', 
-                       '7_businesscards.card_1.next', '7_businesscards.card_bingo.bingo', '7_tomap', '7_worker', '8_logbook', '8_logbook.page.bingo', '8_worker', '9_reader', '9_reader.paper0.next', 
-                       '9_reader.paper1.next', '9_reader.paper2.bingo', '9_toentry', '9_worker', '10_wellsbadge', '10_worker', '11_archivist', '11_journals', '11_journals.hub.topics', 
-                       '11_journals.pic_0.next', '11_journals.pic_1.next', '11_journals.pic_2.bingo', '11_toentry', '11_tostacks', '12_chap2_finale_c', '12_tomap']
-    elif stage == 3:
-        cols_needed = ['13_ch3start', '13_seescratches', '13_toentry', '14_glasses', '14_lockeddoor', '14_teddy', '15_directory', '15_directory.closeup.archivist', '15_glasses', '15_key', 
-                       '16_confrontation', '16_unlockdoor', '17_gramps', '17_savedteddy', '18_boss', '18_coffee', '18_crane_ranger', '18_expert', '18_groupconvo_flag', '18_remove_cup', '18_tomap', 
-                       '18_tracks', '18_tracks.hub.deer', '18_wells', '19_colorbook', '19_flag_girl', '19_tomap', '20_reader_flag', '20_reader_flag.paper0.next', '20_reader_flag.paper2.bingo', 
-                       '20_tomap', '20_worker', '21_archivist_glasses', '21_journals_flag', '21_journals_flag.hub.topics', '21_journals_flag.pic_0.bingo', '21_journals_flag.pic_0.next', '21_toentry', 
-                       '21_tofrontdesk', '21_tostacks', '21_worker', '22_chap4_finale_c', '22_tomap']
+    # elif stage == 2:
+    #     cols_needed = ['5_toentry', '5_what_happened', '6_archivist','6_gramps', '6_magnify', '6_trigger_coffee', '6_trigger_scarf', '7_businesscards', '7_businesscards.card_0.next', 
+    #                    '7_businesscards.card_1.next', '7_businesscards.card_bingo.bingo', '7_tomap', '7_worker', '8_logbook', '8_logbook.page.bingo', '8_worker', '9_reader', '9_reader.paper0.next', 
+    #                    '9_reader.paper1.next', '9_reader.paper2.bingo', '9_toentry', '9_worker', '10_wellsbadge', '10_worker', '11_archivist', '11_journals', '11_journals.hub.topics', 
+    #                    '11_journals.pic_0.next', '11_journals.pic_1.next', '11_journals.pic_2.bingo', '11_toentry', '11_tostacks', '12_chap2_finale_c', '12_tomap']
+    # elif stage == 3:
+    #     cols_needed = ['13_ch3start', '13_seescratches', '13_toentry', '14_glasses', '14_lockeddoor', '14_teddy', '15_directory', '15_directory.closeup.archivist', '15_glasses', '15_key', 
+    #                    '16_confrontation', '16_unlockdoor', '17_gramps', '17_savedteddy', '18_boss', '18_coffee', '18_crane_ranger', '18_expert', '18_groupconvo_flag', '18_remove_cup', '18_tomap', 
+    #                    '18_tracks', '18_tracks.hub.deer', '18_wells', '19_colorbook', '19_flag_girl', '19_tomap', '20_reader_flag', '20_reader_flag.paper0.next', '20_reader_flag.paper2.bingo', 
+    #                    '20_tomap', '20_worker', '21_archivist_glasses', '21_journals_flag', '21_journals_flag.hub.topics', '21_journals_flag.pic_0.bingo', '21_journals_flag.pic_0.next', '21_toentry', 
+    #                    '21_tofrontdesk', '21_tostacks', '21_worker', '22_chap4_finale_c', '22_tomap']
 
-    COLS = []
-    COLS.extend([i + '_action_timesum' for i in cols_needed])
-    COLS.extend([i + '_action_timemean' for i in cols_needed])
-    COLS.extend([i + '_action_timestd' for i in cols_needed])
-    COLS.extend([i + '_action_timemedian' for i in cols_needed])
-    COLS.extend([i + '_action_timemax' for i in cols_needed])
-    COLS.extend([i + '_action_timecount' for i in cols_needed])
+        COLS = []
+        COLS.extend([i + '_action_timesum' for i in cols_needed])
+        COLS.extend([i + '_action_timemean' for i in cols_needed])
+        COLS.extend([i + '_action_timestd' for i in cols_needed])
+        COLS.extend([i + '_action_timemedian' for i in cols_needed])
+        COLS.extend([i + '_action_timemax' for i in cols_needed])
+        COLS.extend([i + '_action_timecount' for i in cols_needed])
 
-    if train == False:
-        missing_cols = np.array(COLS)[~np.isin(COLS, tmp_pivot.columns)]
-        for _col in missing_cols:
-            tmp_pivot[_col] = 0
+        if train == False:
+            missing_cols = np.array(COLS)[~np.isin(COLS, tmp_pivot.columns)]
+            for _col in missing_cols:
+                tmp_pivot[_col] = 0
 
 
-    tmp_pivot = tmp_pivot[COLS]
-    tmp_pivot = tmp_pivot.reset_index()
-    _train = pd.merge(left=_train, right=tmp_pivot, on='session_id', how='left')
+        tmp_pivot = tmp_pivot[COLS]
+        tmp_pivot = tmp_pivot.reset_index()
+        _train = pd.merge(left=_train, right=tmp_pivot, on='session_id', how='left')
     
     
     # Time per level and room
@@ -345,53 +324,6 @@ def get_general_features_2(df, stage, train=True):
         COLS.extend([str(level) + '_' + i + '_' + 'action_timemax' for i in cols])
         COLS.extend([str(level) + '_' + i + '_' + 'action_timecount' for i in cols])
 
-
-    if train == False:
-        missing_cols = np.array(COLS)[~np.isin(COLS, tmp_pivot.columns)]
-        for _col in missing_cols:
-            tmp_pivot[_col] = 0
-
-
-    tmp_pivot = tmp_pivot[COLS]
-    tmp_pivot = tmp_pivot.reset_index()
-    _train = pd.merge(left=_train, right=tmp_pivot, on='session_id', how='left')
-    
-    
-    # Time per level and text_fqid
-    tmp = df.groupby(['session_id', 'level', 'text_fqid']).agg({'action_time' : ['sum', 'mean', 'std', 'median', 'max', 'count']})
-    tmp.columns = tmp.columns.map(''.join)
-    tmp_pivot = tmp.pivot_table(index='session_id', columns=['level', 'text_fqid'], values=['action_timesum', 'action_timemean', 'action_timestd', 'action_timemedian', 'action_timemax', 
-                                                                                            'action_timecount'])
-    tmp_pivot.columns = [str(i[1]) + '_' + i[2] + '_' + i[0] for i in tmp_pivot.columns]
-
-    if stage == 1:
-        cols_needed = ['0_tunic.historicalsociety.closet.gramps.intro_0_cs_0', '0_tunic.historicalsociety.closet.teddy.intro_0_cs_0', '1_tunic.historicalsociety.entry.groupconvo', 
-                       '2_tunic.historicalsociety.collection.cs', '2_tunic.historicalsociety.collection.gramps.found', '2_tunic.historicalsociety.collection.tunic.slip', 
-                       '3_tunic.kohlcenter.halloffame.plaque.face.date', '3_tunic.kohlcenter.halloffame.togrampa']
-
-    elif stage == 2:
-        cols_needed = ['5_tunic.historicalsociety.closet_dirty.what_happened', '6_tunic.historicalsociety.closet_dirty.gramps.news', '6_tunic.historicalsociety.closet_dirty.trigger_coffee', 
-                       '6_tunic.historicalsociety.closet_dirty.trigger_scarf', '6_tunic.historicalsociety.frontdesk.archivist.have_glass', '6_tunic.historicalsociety.frontdesk.archivist.hello', 
-                       '7_tunic.humanecology.frontdesk.worker.intro', '8_tunic.drycleaner.frontdesk.logbook.page.bingo', '8_tunic.drycleaner.frontdesk.worker.hub', 
-                       '9_tunic.drycleaner.frontdesk.worker.done', '9_tunic.library.frontdesk.worker.hello', '9_tunic.library.microfiche.reader.paper2.bingo', '10_tunic.library.frontdesk.worker.wells', 
-                       '11_tunic.historicalsociety.frontdesk.archivist.newspaper', '11_tunic.historicalsociety.stacks.journals.pic_2.bingo']
-
-    elif stage == 3:
-        cols_needed = ['13_tunic.historicalsociety.basement.ch3start', '13_tunic.historicalsociety.basement.seescratches', '14_tunic.historicalsociety.cage.teddy.trapped', 
-                       '15_tunic.historicalsociety.cage.glasses.afterteddy', '15_tunic.historicalsociety.entry.directory.closeup.archivist', '16_tunic.historicalsociety.cage.confrontation', 
-                       '16_tunic.historicalsociety.cage.unlockdoor', '17_tunic.historicalsociety.basement.savedteddy', '17_tunic.historicalsociety.collection_flag.gramps.flag', 
-                       '18_tunic.historicalsociety.entry.boss.flag', '18_tunic.historicalsociety.entry.groupconvo_flag', '18_tunic.historicalsociety.entry.wells.flag', 
-                       '18_tunic.wildlife.center.crane_ranger.crane', '18_tunic.wildlife.center.expert.removed_cup', '18_tunic.wildlife.center.wells.animals', '18_tunic.wildlife.center.wells.nodeer', 
-                       '19_tunic.flaghouse.entry.flag_girl.hello', '19_tunic.flaghouse.entry.flag_girl.symbol', '20_tunic.library.frontdesk.worker.flag', 
-                       '21_tunic.historicalsociety.frontdesk.archivist_glasses.confrontation', '21_tunic.historicalsociety.stacks.journals_flag.pic_0.bingo', '21_tunic.library.frontdesk.worker.nelson']
-        
-    COLS = []
-    COLS.extend([i + '_action_timesum' for i in cols_needed])
-    COLS.extend([i + '_action_timemean' for i in cols_needed])
-    COLS.extend([i + '_action_timestd' for i in cols_needed])
-    COLS.extend([i + '_action_timemedian' for i in cols_needed])
-    COLS.extend([i + '_action_timemax' for i in cols_needed])
-    COLS.extend([i + '_action_timecount' for i in cols_needed])
 
     if train == False:
         missing_cols = np.array(COLS)[~np.isin(COLS, tmp_pivot.columns)]
